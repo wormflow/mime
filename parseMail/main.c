7b4894c327b3b925e3320062940662fcf7eeebde
feng <feng@fengdeMacBook-Pro.local>
2015-08-30 22:11:22 +0800

    Initial Commit

---
+++ parseMail/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  parseMail
+//
+//  Created by feng on 15/8/30.
+//  Copyright (c) 2015年 feng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
